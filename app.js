c6b8300b22a430291f17c4e887214cc7cff6230c
janrikard <102407793+janrikard@users.noreply.github.com>
2023-11-18 21:41:46 +0100

    12 hour clock,seconds,unixtime

--- app.js
+++ app.js
@@ -38,6 +38,39 @@ class MyApp extends Homey.App {
       }
     })
 
+    const secondcard = this.homey.flow.getActionCard('Second_As_A_Number');
+    secondcard.registerRunListener(async (args) => {
+      let seconddate = new Date();
+      let current_second = seconddate.getSeconds();
+     
+      return {
+        Current_second_output: current_second
+      }
+    })
+
+    const unixtimecard = this.homey.flow.getActionCard('Unix_Time_As_A_Number');
+    unixtimecard.registerRunListener(async (args) => {
+      
+      return {
+        Current_unix_time_output: Date.now()
+      }
+    })
+  const hour12card = this.homey.flow.getActionCard('hour_12_clock_As_A_Number');
+    hour12card.registerRunListener(async (args) => {
+      let hour12date = new Date();
+      let current_hour12 = hour12date.getHours();
+      let period = "am";
+      current_hour12=current_hour12+1;
+      if (current_hour12>12){
+        period="pm";
+        current_hour12=current_hour12-12;
+      }
+
+      return {
+        Current_12_hour_output: current_hour12,
+        Current_period_output: period
+      }
+    })
   }
 
 }
